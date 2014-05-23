273907dd60761b6efc1e4b192418e99c6cfde9e3
emad_n <navid.emad@epitech.eu>
2014-05-23 18:02:52 +0200

    fix my_putchar && add some functions

---
+++ libc.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int		_putchar(int c)
+{
+	return write(1, &c, 1) ? c : EOF;
+}
+
+size_t	_strlen(const char *s)
+{
+	return *s ? _strlen(s + 1) + 1 : 0;
+}
+
+void	_puts(const char *s)
+{
+	*s && write(1, &(*s), 1) ? _puts(s + 1) : 0;
+}
+
+int 	_strcmp(const char *s1, const char *s2)
+{
+	return *s1 == *s2 && *s2 ? *s1 == *s2 ? _strcmp(++s1, ++s2) : 1 : 0;
+}
+
+int 	_strncmp(const char *s1, const char *s2, size_t n)
+{
+	return *s1 == *s2 && *s2 ? *s1 == *s2 && n ? _strncmp(++s1, ++s2, --n) : 1 : 0;
+}
+
+double	_pow(double x, double n)
+{
+	return n > 0 ? x * _pow(x, n - 1) : 1;
+}
+
+int 	_atoi(const char *s, int res)
+{
+	return *s ? *s == '-' ? -_atoi(s + 1, res) : _atoi(s + 1, 10 * res + *s - '0') : res;
+}
+
+int main(void)
+{
+	printf("%d => '%d'\n", 120233, _atoi("120233", 0));
+	printf("%d => '%d'\n", -120233, _atoi("-120233", 0));
+	printf("%d => '%d'\n", 123, _atoi("123", 0));
+	printf("%d => '%d'\n", -23, _atoi("-23", 0));
+	printf("%d => '%d'\n", -0, _atoi("-0", 0));
+	printf("%d => '%d'\n", 0, _atoi("0", 0));
+	return (0);
+}
